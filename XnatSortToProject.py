35fb0d1ffe22078e3e7a0cfcd920d118b991658d
karthikmasi <karthik.ramadass@vanderbilt.edu>
2019-11-20 14:04:26 -0600

    REDCap access with key called. Argparse setup.

--- XnatSortToProject.py
+++ XnatSortToProject.py
@@ -13,6 +13,7 @@ import redcap
 import dax
 import sys
 import logging as LOGGER
+import argparse
 
 def redcap_project_access(API_KEY):
     """
@@ -27,3 +28,23 @@ def redcap_project_access(API_KEY):
         sys.exit(1)
     return project
 
+def add_to_parser():
+    """
+
+    :return: parser object
+    """
+    parser = argparse.ArgumentParser(description='Xnat Native Project Sort')
+    parser.add_argument("-k","--key",dest='API_KEY',default=None, required=True,
+                        help='API key to REDCap Database')
+    return parser
+def execute():
+    """
+    point of execution
+    :return: No return. Only stdout to LOGGER
+    """
+    parser = add_to_parser()
+    OPTIONS = parser.parse_args()
+    project = redcap_project_access(OPTIONS.API_KEY)
+
+if __name__ == '__main__':
+    execute()
