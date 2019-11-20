ecbdb612fad21479b7b5c2c63b64e5d29fd2b4d4
karthikmasi <karthik.ramadass@vanderbilt.edu>
2019-11-20 10:12:15 -0600

    Added REDCap access

--- XnatSortToProject.py
+++ XnatSortToProject.py
@@ -14,3 +14,16 @@ import dax
 import sys
 import logging as LOGGER
 
+def redcap_project_access(API_KEY):
+    """
+    Access point to REDCap form
+    :param API_KEY:string with REDCap database API_KEY
+    :return: redcap Project Object
+    """
+    try:
+        project = redcap.Project('https://redcap.vanderbilt.edu/api/', API_KEY)
+    except:
+        LOGGER.error('ERROR: Could not access redcap. Either wrong API_URL/API_KEY or redcap down.')
+        sys.exit(1)
+    return project
+
